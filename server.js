3082937c07619c8468a373da71f40e5ffcddb429
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-12 17:34:21 -0700

    update server: remove port

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const http = require("http");
 
-const PORT = process.env.PORT || 5000;
-
 http.createServer((req, res) => {
   console.log(`Server running on port ${PORT}`);
-}).listen(PORT);
+}).listen();
