8845c6a220f88e075a06c66fb9a69d835270a491
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-12 19:44:40 -0700

    update server: create send request

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const http = require("http");
 
 const db = require("./db.service");
 
+function sendRequest(res, code, headers={ "Access-Control-Allow-Origin": "*" }, data) {
+  res.writeHead(code, headers);
+  res.write(JSON.stringify(data));
+  res.end();
+}
+
 http.createServer((req, res) => {
   db.query("SELECT * FROM Patients")
     .then(value => {
-      res.end(value);
+      sendRequest(res, 200, { "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" }, value);
     })
     .catch(err => {
-      res.end(err);
+      sendRequest(res, 404, { "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" }, err);
     })
 }).listen();
