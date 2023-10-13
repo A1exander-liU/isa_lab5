ebce7b5ddbe6e3ba95f5748ea7b5aaf0996c6dbd
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-12 20:53:28 -0700

    update sql controller: add get logic

--- routes/sql.controller.js
+++ routes/sql.controller.js
@@ -1,9 +1,19 @@
 const url = require("url");
 
+const utils = require("../utils");
+const constants = require("../constants");
+const sqlService = require("../services/sql.service");
+
 function sql(path, req, res) {
-  switch (path) {
-    case "": {
-      
+  switch(req.method) {
+    case "GET": {
+      sqlService.query(path)
+        .then(data => {
+          utils.response(200, constants.defaultHeaders, res, JSON.stringify(data));
+        })
+        .catch(err => {
+          utils.response(200, constants.defaultHeaders, res, JSON.stringify(err));
+        });
     }
   }
 }
