f17fb48e122016d3e191bf52efc00a6ef53129fd
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-12 20:22:32 -0700

    add constant: allowOriginKey

--- constants.js
+++ constants.js
@@ -1,4 +1,7 @@
 const basePath = "/COMP4537/labs/5/api";
+const allowOriginKey = "Access-Control-Allow-Origin";
+
 module.exports = {
-  basePath: basePath
+  basePath: basePath,
+  allowOriginKey: allowOriginKey
 };
