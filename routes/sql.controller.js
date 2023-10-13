0ee5baab2b3c318e92278a8932908d9e155be86e
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-12 21:54:01 -0700

    update sql controller: add post path

--- routes/sql.controller.js
+++ routes/sql.controller.js
@@ -17,6 +17,23 @@ function sql(path, req, res) {
           utils.response(200, constants.defaultHeaders, res, JSON.stringify(err));
         });
     }
+    case "POST": {
+      const rows = [
+        "Sara Brown", "1901-01-01",
+        "John Smith", "1941-01-01",
+        "Jack Ma", "1961-01-30",
+        "Elon Musk", "1999-01-01"
+      ];
+      sqlService.insert("insert into Patients (name, dateofbirth) VALUES (?, ?), (?, ?), (?, ?), (?, ?)", rows)
+        .then(data => {
+          console.log(`controller ITEM: ${data}`);
+          utils.response(200, constants.defaultHeaders, res, JSON.stringify(data));
+        })
+        .catch(err => {
+          console.log(`controller ERR: ${err}`);
+          utils.response(200, constants.defaultHeaders, res, JSON.stringify(err));
+        })
+    }
   }
 }
 
