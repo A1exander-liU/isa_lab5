b6af28ae49e5075ef67c8a83de4070bec4c9711a
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-12 19:02:39 -0700

    update server: remove stringify

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const db = require("./db.service");
 http.createServer((req, res) => {
   db.query("SELECT * FROM Patients")
     .then(value => {
-      res.end(JSON.stringify(`${value}`));
+      res.end(value);
     })
     .catch(err => {
-      res.end(JSON.stringify(`${err}`));
+      res.end(err);
     })
 }).listen();
