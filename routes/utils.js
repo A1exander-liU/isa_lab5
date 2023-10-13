a7941a4fc80b490e5a854473a314a5ffd0952bdc
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-12 20:20:00 -0700

    add util: response

--- routes/utils.js
+++ routes/utils.js
@@ -2,6 +2,13 @@ function parseUrlSqlQuery(sqlQueryUrl) {
   return sqlQueryUrl.replace(/%20/g, " ");
 }
 
+function response(statusCode, headers={}, res, data) {
+  res.writeHead(statusCode, headers);
+  res.write(JSON.stringify(data));
+  res.end();
+}
+
 module.exports = {
-  parseUrlSqlQuery: parseUrlSqlQuery
+  parseUrlSqlQuery: parseUrlSqlQuery,
+  response: response
 };
